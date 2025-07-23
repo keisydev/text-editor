5a9056c8c1d254807cc7ab120bfd220541605cbf
Keisy Rathlef <keisy.dev@gmail.com>
2025-07-23 21:20:00 +0100

    fix: corrige problema de roteamento com HashRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 // client/src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar'; // Componente de navegação
 import Home from './pages/Home';
 import Editor from './pages/Editor';
@@ -8,7 +8,7 @@ import '../../client/src/App.css'; // Estilos globais
 
 function App() {
   return (
-    <Router basename="/text-editor"> 
+    <Router> 
       <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
